642f6aa0c7a076e3a88bc9875d74253d4372d4de
Jorge Anaya <jjam1190@gmail.com>
2021-03-21 01:07:47 -0600

    Redux Thunk

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,6 +1,11 @@
-export const traerTodos = () => (dispatch) => {
-    dispatch({
-      type: 'traer_usuarios',
-      payload: [1,2,3]
-    })
-}
+import axios from 'axios';
+
+export const traerTodos = () => async dispatch => {
+  const respuesta = await axios.get (
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  dispatch ({
+    type: 'traer_usuarios',
+    payload: respuesta.data,
+  });
+};
