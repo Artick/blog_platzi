1603bee0f75718325bab0c49bb27fc0940aff42f
Jorge Anaya <jjam1190@gmail.com>
2021-03-21 01:27:23 -0600

    Archivos Types

--- src/reducers/usuariosReducer.js
+++ src/reducers/usuariosReducer.js
@@ -1,14 +1,16 @@
+import {TRAER_TODOS} from '../types/usuariosTypes';
+
 const INITIAL_STATE = {
-  usuarios: [1, 2, 3],
+  usuarios: [],
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'traer_usuarios':
+    case TRAER_TODOS:
       return {...state, usuarios: action.payload};
 
     default:
       return state;
-  };
+  }
 };
