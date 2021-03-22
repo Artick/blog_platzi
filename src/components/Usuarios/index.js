bc29710656bc0aedae6e1f637a386f2a38d8e747
Jorge Anaya <jjam1190@gmail.com>
2021-03-22 01:37:56 -0600

    Escenarios Asincronos

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -24,6 +24,8 @@ class Usuarios extends Component {
     ));
 
   render () {
+    // console.log (this.props.cargando);
+    // console.log (this.props.error);
     return (
       <div>
         <table className="tabla">
