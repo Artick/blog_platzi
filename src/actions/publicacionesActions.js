71de9255541491931a5f8b61368f06e0a35478ca
Jorge Anaya <jjam1190@gmail.com>
2021-03-23 14:11:28 -0600

    Llamando multiples reducers en una acción

---
+++ src/actions/publicacionesActions.js
@@ -0,0 +1,24 @@
+import {TRAER_TODOS, CARGANDO, ERROR} from '../types/publicacionesTypes';
+import axios from 'axios';
+
+export const traerTodos = () => async dispatch => {
+  dispatch ({
+    type: CARGANDO,
+  });
+
+  try {
+    const respuesta = await axios.get (
+      'http://jsonplaceholder.typicode.com/posts'
+    );
+    dispatch ({
+      type: TRAER_TODOS,
+      payload: respuesta.data,
+    });
+  } catch (error) {
+    console.log ('Error: ', error.message);
+    dispatch ({
+      type: ERROR,
+      payload: 'Algo salió mal, intente más tarde',
+    });
+  }
+};
